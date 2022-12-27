f6a0f1acd96fa32814078ae2ad729169c200f002
pwbear12300 <timewornbn@gmail.com>
2022-12-27 08:42:10 -0300

    create a project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './navbar.scss';
+
+const Navbar = () => {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../components/Navbar';
+
+import './home.scss';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Home;
